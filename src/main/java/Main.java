d3266d358db44608898be02d855139797e6ce5cc
Wei Zhou <leonzhouwei@gmail.com>
2017-07-02 09:28:31 +0800

    feat: make the demo better

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,50 @@
-public class Main {  
-	public static void main(String args[]) {
-		int n = 1;
-		int sum = -1;
-		while (true) {
-			sum = sum(n);
-			System.out.printf("1+2+3+...+%d = %d\n", n, sum);
-			n += 1;
-		}
-        }
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
-	private static int sum(final int n) {
-		int sum = 0;
-		for (int i = 0; i < n; ++i) {
-			sum += i;
-		}
-		return sum;
+public class Main {
+
+	private static final int N = 5_000_000;
+
+	private boolean useArrayList = false;
+	private List<String> list;
+
+	public static void main(String args[]) {
+		Main main = new Main(true);
+		main.init();
+		main.loop();
 	}
+
+	public Main() {
+	}
+
+	public Main(boolean useArrayList) {
+		this.useArrayList = useArrayList;
+	}
+
+	public void init() {
+		if (useArrayList) {
+			list = new ArrayList<String>(N);
+		} else {
+			list = new LinkedList<String>();
+		}
+
+		System.out.println("--- init started ...");
+		for (int i = 0; i < N; ++i) {
+			list.add(Integer.toString(i));
+		}
+		System.out.println("--- init done");
+	}
+
+	public void loop() {
+		double rand;
+		int i;
+		while (true) {
+			rand = Math.random();
+			i = (int) (rand * N);
+			System.out.printf("list[%d]=%s\n", i, list.get(i));
+		}
+	}
+
 }
 
